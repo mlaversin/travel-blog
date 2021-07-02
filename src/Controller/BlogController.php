a52743be09a9a2a7c1a5c2a60f7809fb626a7a88
Morgan Laversin <morganlaversin@gmail.com>
2021-07-02 13:27:04 +0200

    abstacts on articles page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
@@ -20,10 +21,8 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-
         $articles = $repo->findAll();
 
         return $this->render('blog/index.html.twig', [
@@ -34,9 +33,8 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show($id)
+    public function show(ArticleRepository $repo, $id) 
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
         $article = $repo->find($id);
 
         return $this->render('blog/show.html.twig', [
