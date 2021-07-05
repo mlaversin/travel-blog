2052da4303d219f4981a30c3ba36b321070e8577
Morgan Laversin <morganlaversin@gmail.com>
2021-07-05 10:19:45 +0200

    edit article form

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index(ArticleRepository $repo): Response
+    {
+        $articles = $repo->findAll();
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/article/new", name="article_create")
+     */
+    public function create(Request $request, EntityManagerInterface $manager) {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title')
+                     ->add('content')
+                     ->add('image')
+                     ->getForm();
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $article->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToroute('article_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('article/create.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/article/edit/{id}", name="article_edit")
+     */
+    public function edit(Article $article, Request $request, EntityManagerInterface $manager) {
+       
+        $form = $this->createFormBuilder($article)
+                     ->add('title')
+                     ->add('content')
+                     ->add('image')
+                     ->getForm();
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToroute('article_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('article/create.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show(ArticleRepository $repo, $id) 
+    {
+        $article = $repo->find($id);
+
+        return $this->render('article/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+}
