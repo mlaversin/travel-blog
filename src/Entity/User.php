6dcc040f0fc524fbf9a4b24c14d0d73369aa2b93
Morgan Laversin <morganlaversin@gmail.com>
2021-07-13 18:23:55 +0200

    authentication controller

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="L'email que vous avez indiqué est déjà utilisé !")
  */
 class User implements UserInterface, \Serializable
 {
